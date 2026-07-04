71fc8133d8009eb527b9718a1e3033c6b1b327df
rizwanrko77 <rizwanrko77@gmail.com>
2026-07-04 12:09:30 +0530

    Update bio in index.html

--- js/main.js
+++ js/main.js
@@ -96,7 +96,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         }
       });
     }, {
-      threshold: 0.1,
+      threshold: 0,
       rootMargin: '0px 0px -40px 0px'
     });
 
